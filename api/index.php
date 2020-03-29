3449465377c3ad9c7ae3759df69740c6ae3fdc93
Zach <pro.bizach@gmail.com>
2020-03-29 14:47:48 +0200

    Added all

--- api/index.php
+++ api/index.php
@@ -3,12 +3,12 @@
 /**
  * This Will take the Last part of the url
  */
+header("Access-Control-Allow-Origin: http://localhost:3000");
 require_once("./autoloader.php");
-define('BASE_URI', str_replace('\\','/', substr(__DIR__,strlen($_SERVER['DOCUMENT_ROOT']))));
-$uri = str_replace(BASE_URI,'',$_SERVER['REQUEST_URI']);
+define('BASE_URI', str_replace('\\', '/', substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT']))));
+$uri = str_replace(BASE_URI, '', $_SERVER['REQUEST_URI']);
 
 // echo $uri;
 
 $router = new Router();
 $router->route($uri);
-
